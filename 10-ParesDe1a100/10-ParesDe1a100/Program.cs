bd7f367e615ba2b9a5d6821d351a0f617c259795
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:26:11 -0300

    Add files via upload

---
+++ 10-ParesDe1a100/10-ParesDe1a100/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _10_ParesDe1a100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, a;
+            a = 0;
+
+            for (i = 1; i <= 100; i++)
+            {
+
+
+
+                if (i % 2 == 0)
+                {
+
+                    a = a + 1;
+                    Console.WriteLine("Los numeros pares son: " + i);
+
+                }
+            }
+            Console.WriteLine("La cantidad de numeros pares es de: " + a);
+            Console.ReadKey();
+        }
+    }
+}
